4871326731e926b8ee9135d19381706b12b6bfb0
Wang Yudong <wangyudong.blue@163.com>
2015-09-18 14:50:58 +0800

    Initial Commit

---
+++ R3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  R3
+//
+//  Created by wyudong on 15/9/18.
+//  Copyright © 2015年 wyudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
