ba9cd0f763c3a6deda7eb38acd51601af18756b7
Wang Yudong <wangyudong.blue@163.com>
2015-09-21 20:11:39 +0800

    Add drawing

--- R3/ViewController.m
+++ R3/ViewController.m
@@ -13,6 +13,10 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) CMMotionManager *motionManager;
+@property (nonatomic, strong) SCNView *sceneView;
+@property (nonatomic, strong) SCNNode *cameraNode;
+@property (nonatomic, strong) SCNNode *currentDrawingNode;
+@property (nonatomic, strong) CAShapeLayer *currentDrawingLayer;
 
 @end
 
@@ -20,33 +24,34 @@
 
 static CGFloat initialAttitudeRoll;
 static CGFloat initialAttitudePitch;
-const CGFloat cameraDistance = 2.0;
+const CGFloat kCameraDistance = 2.0;
+const CGFloat kCurrentDrawingLayerSize = 512.0;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
     // Scene initialization
-    SCNView *sceneView = [[SCNView alloc] initWithFrame:self.view.frame];
+    self.sceneView = [[SCNView alloc] initWithFrame:self.view.frame];
     //SceneKitView.backgroundColor = [UIColor blackColor];
-    sceneView.scene = [SCNScene scene];
-    [self.view addSubview:sceneView];
+    self.sceneView.scene = [SCNScene scene];
+    [self.view addSubview:self.sceneView];
     
     SCNNode *centerNode = [SCNNode node];
     centerNode.position = SCNVector3Zero;
-    [sceneView.scene.rootNode addChildNode:centerNode];
+    [self.sceneView.scene.rootNode addChildNode:centerNode];
     
     // Camera
     SCNCamera *camera = [SCNCamera camera];
     camera.xFov = 20.0;
     camera.yFov = 20.0;
     
-    SCNNode *cameraNode = [SCNNode node];
-    cameraNode.camera = camera;
-    [sceneView.scene.rootNode addChildNode:cameraNode];
+    self.cameraNode = [SCNNode node];
+    self.cameraNode.camera = camera;
+    [self.sceneView.scene.rootNode addChildNode:self.cameraNode];
     
-    cameraNode.constraints = @[[SCNLookAtConstraint lookAtConstraintWithTarget:centerNode]];
-    cameraNode.pivot = SCNMatrix4MakeTranslation(0, 0, -cameraDistance);
+    self.cameraNode.constraints = @[[SCNLookAtConstraint lookAtConstraintWithTarget:centerNode]];
+    self.cameraNode.pivot = SCNMatrix4MakeTranslation(0, 0, -kCameraDistance);
     
     // Motion manager
     NSOperationQueue *queue = [[NSOperationQueue alloc] init];
@@ -58,17 +63,95 @@ const CGFloat cameraDistance = 2.0;
         [self.motionManager startDeviceMotionUpdatesToQueue:queue
                                                 withHandler:^(CMDeviceMotion * _Nullable deviceMotion, NSError * _Nullable error) {
             // Motion process
-                                                    static dispatch_once_t once;
-                                                    dispatch_once(&once, ^{
-                                                        initialAttitudeRoll = deviceMotion.attitude.roll;
-                                                        initialAttitudePitch = deviceMotion.attitude.pitch;
-                                                    });
+            static dispatch_once_t once;
+            dispatch_once(&once, ^{
+                initialAttitudeRoll = deviceMotion.attitude.roll;
+                initialAttitudePitch = deviceMotion.attitude.pitch;
+            });
+            
+            CGFloat deltaRoll = initialAttitudeRoll - deviceMotion.attitude.roll;
+            CGFloat deltaPitch = (initialAttitudePitch - deviceMotion.attitude.pitch);
                                                     
             [[NSOperationQueue mainQueue] addOperationWithBlock:^{
                 // Update UI
+                self.cameraNode.eulerAngles = SCNVector3Make(deltaPitch, deltaRoll, 0);
+                //NSLog(@"camera(x, y, z): (%.2f, %.2f, %.2f)", self.cameraNode.eulerAngles.x, self.cameraNode.eulerAngles.y, self.cameraNode.eulerAngles.z);
             }];
         }];
     }
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    
+    self.currentDrawingNode = [SCNNode nodeWithGeometry:[SCNBox boxWithWidth:1.0 height:1.0 length:0 chamferRadius:0]];
+    self.currentDrawingLayer = [CAShapeLayer layer];
+    
+    SCNNode *currentDrawingNode = self.currentDrawingNode;
+    CAShapeLayer *currentDrawingLayer = self.currentDrawingLayer;
+    
+    if (currentDrawingLayer && currentDrawingNode) {
+        NSLog(@"began");
+        currentDrawingNode.position = SCNVector3Zero;
+        currentDrawingNode.eulerAngles = self.cameraNode.eulerAngles;
+        [self.sceneView.scene.rootNode addChildNode:currentDrawingNode];
+        
+        currentDrawingLayer.strokeColor = [UIColor grayColor].CGColor;
+        currentDrawingLayer.fillColor = nil;
+        currentDrawingLayer.lineWidth = 5.0;
+        currentDrawingLayer.lineJoin = kCALineJoinRound;
+        currentDrawingLayer.lineCap = kCALineCapRound;
+        currentDrawingLayer.frame = CGRectMake(0, 0, kCurrentDrawingLayerSize, kCurrentDrawingLayerSize);
+        
+        SCNMaterial *material = [SCNMaterial material];
+        material.diffuse.contents = currentDrawingLayer;
+        material.lightingModelName = SCNLightingModelConstant;
+        
+        currentDrawingNode.geometry.materials = @[material];
+    }
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesMoved:touches withEvent:event];
+    
+    CGPoint locationInView = [[touches anyObject] locationInView:self.view];
+    NSArray *hitTestResultArray = [self.sceneView hitTest:locationInView options:nil];
+    CAShapeLayer *currentDrawingLayer = self.currentDrawingLayer;
+    
+    // Filter
+    NSPredicate *drawingNode = [NSPredicate predicateWithBlock:^BOOL(id  _Nonnull evaluatedObject, NSDictionary<NSString *,id> * _Nullable bindings) {
+        if ([evaluatedObject isKindOfClass:[SCNHitTestResult class]]) {
+            SCNNode *node = ((SCNHitTestResult *)evaluatedObject).node;
+            return (node == self.currentDrawingNode);
+        } else {
+            return nil;
+        }
+    }];
+    SCNHitTestResult *hitTestResult = [hitTestResultArray filteredArrayUsingPredicate:drawingNode].firstObject;
+    
+    if (hitTestResult && currentDrawingLayer) {
+        if (currentDrawingLayer.path == nil) {
+            CGFloat newX = (hitTestResult.localCoordinates.x + 0.5) * kCurrentDrawingLayerSize;
+            CGFloat newY = (hitTestResult.localCoordinates.y + 0.5) * kCurrentDrawingLayerSize;
+            currentDrawingLayer.path = [UIBezierPath bezierPathWithRect:CGRectMake(newX, newY, 0, 0)].CGPath;
+        }
+        
+        UIBezierPath *drawingPath = [UIBezierPath bezierPathWithCGPath:currentDrawingLayer.path];
+        CGFloat newX = (hitTestResult.localCoordinates.x + 0.5) * kCurrentDrawingLayerSize;
+        CGFloat newY = (hitTestResult.localCoordinates.y + 0.5) * kCurrentDrawingLayerSize;
+        [drawingPath addLineToPoint:CGPointMake(newX, newY)];
+        currentDrawingLayer.path = drawingPath.CGPath;
+    }
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesEnded:touches withEvent:event];
+    
+    self.currentDrawingNode = nil;
+    self.currentDrawingLayer = nil;
 }
 
 @end
